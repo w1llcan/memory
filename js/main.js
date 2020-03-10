9263e4807f5bbddf4a4f247d8260cf32f38e2e69
w1llcan <38799476+w1llcan@users.noreply.github.com>
2020-03-09 22:39:20 -0400

    added functions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
-console.log("Game is Up and running!");
-
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne +"!");
-console.log("User flipped " + cardTwo +"!");
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[2]) {
-		alert("You found a match!");
-} else if (cardsInPlay[0] != cardsInPlay[2]) {
-	alert("Sorry, try again.")
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) console.log("You found a match!");
+  else console.log("Sorry, try again.")
 }
+
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId] +"!");
+	cardsInPlay.push(cards[cardId]);	
 }
+
+flipCard(2);
+flipCard(0);
+checkForMatch();
