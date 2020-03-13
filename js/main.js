5abedb479d9f21512cee10996e32c0883a0ec468
w1llcan <38799476+w1llcan@users.noreply.github.com>
2020-03-13 13:56:15 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+];
+
 const cardsInPlay = [];
 
 function checkForMatch() {
@@ -8,10 +30,27 @@ function checkForMatch() {
 
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId] +"!");
-	cardsInPlay.push(cards[cardId]);	
+	let cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	//console.log("User flipped " + cards[cardId].rank +"!");
+	  if (cardsInPlay.length === 2) {
+	  	checkForMatch();
+	  }
+		
+};
+
+console.log(cardsInPlay);
+
+function createBoard() {
+	const cardTable = document.getElementById('game-board');
+	for (let i = 0; i < arrayName.length; i++) {
+		let cardElement = document.createElement('img');
+		  cardElement.setAttribute('src', 'images/back.png');
+		  cardElement.setAttribute('data-id', i);
+		  cardElement.addEventListener('click', flipCard);
+		  cardTable.appendChild(cardElement);
+	}
 }
 
-flipCard(2);
-flipCard(0);
-checkForMatch();
+createBoard();
